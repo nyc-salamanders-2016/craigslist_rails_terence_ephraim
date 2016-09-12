0a05da9e2b3f59c70ce40950bcb65c1504ed6fe2
Ephraim Friedman & Terence Stone <pair+ephraimfriedman.terencestone@devbootcamp.com>
2016-09-12 16:07:40 -0400

    Complete new, and show routes for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,51 @@
 class ArticlesController < ApplicationController
+  def index
+    @category = Category.find(params[:category_id])
+    redirect_to category_path(@category)
+  end
+
+
+  def new
+    @category = Category.find(params[:category_id])
+    @article = Article.new
+  end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @article = @category.articles.new(article_params)
+
+    if @article.save
+      @link = "/categories/#{@category.id}/articles/#{@article.id}/edit?key=#{@article.link}"
+      # redirect_to category_article_path(@category, @article)
+      render 'show'
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @category = Category.find(params[:category_id])
+    @article = Article.find(params[:id])
+
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+    if params[:key] && params[:key] == @article.link
+      render 'edit'
+    else
+      not_found
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body, :price, :email)
+    end
 end
