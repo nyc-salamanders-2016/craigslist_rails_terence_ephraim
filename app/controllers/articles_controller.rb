894b4401a0a7d3c3b5538c3805157d9e952476e0
Ephraim Friedman & Terence Stone <pair+ephraimfriedman.terencestone@devbootcamp.com>
2016-09-12 16:32:58 -0400

    Complete challenege, prepare to deploy to heroku

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,22 +26,40 @@ class ArticlesController < ApplicationController
   def show
     @category = Category.find(params[:category_id])
     @article = Article.find(params[:id])
-
   end
 
   def edit
+    @category = Category.find(params[:category_id])
     @article = Article.find(params[:id])
-    if params[:key] && params[:key] == @article.link
-      render 'edit'
+    if params[:key]
+      if params[:key] == @article.link
+        render 'edit'
+      else
+        not_found
+      end
     else
       not_found
     end
   end
 
   def update
+    @category = Category.find(params[:category_id])
+    @article = Article.find(params[:id])
+    @article.assign_attributes(article_params)
+
+    if @article.save
+      redirect_to category_article_path(@category, @article)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @category = Category.find(params[:category_id])
+    @article = Article.find(params[:id])
+
+    @article.destroy
+    redirect_to category_path(@category)
   end
 
   private
